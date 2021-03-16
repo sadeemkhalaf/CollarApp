79f00efbc9339d5c69a1ec3a4aba9588d917deae
Sadeem Ahmad <sadeem.ahmad@pio-tech.com>
2021-03-16 03:39:57 +0200

    still have to reflect changes on Rings

--- src/components/ActivityWidgit.component.tsx
+++ src/components/ActivityWidgit.component.tsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Dimensions, ImageRequireSource, TouchableOpacity } from 'react-native'
 import { SharedStyles } from '.';
-import { CustomActiviyRings, IRing } from './appleActivity'
+import { CustomActiviyRings } from './appleActivity'
 import { CircleImage, Col, Input, InputWithOverlayContainer, PageWrapper, Row, StyledView, Text } from './shared.components'
 const { width } = Dimensions.get("window");
 const huskey = require('../../assets/imgs/husky.png') as ImageRequireSource;
 const close = require('../../assets/imgs/close.png') as ImageRequireSource;
 
 // props for the custom Activity Ring
-const rings: IRing[] = [{
-    start: "#FFF5ED",
-    end: "#FF821C",
-    bg: "#FFF5ED",
-    theta: 0.77
-}, {
-    start: "#D1EDEC",
-    end: "#00BFBF",
-    bg: "#D1EDEC",
-    theta: 0.83
-}, {
-    start: "#D7F1FF",
-    end: "#48BFFD",
-    bg: "#D7F1FF",
-    theta: 0.4
-}];
+const rings = {
+    data: [{
+        start: "#FFF5ED",
+        end: "#FF821C",
+        bg: "#FFF5ED",
+        theta: 0.27
+    }, {
+        start: "#D1EDEC",
+        end: "#00BFBF",
+        bg: "#D1EDEC",
+        theta: 0.43
+    }, {
+        start: "#D7F1FF",
+        end: "#48BFFD",
+        bg: "#D7F1FF",
+        theta: 0.73
+    }], summary: { walk: 12, play: 5, run: 8 }
+    , required: { total: 70, walk: 0.4, play: 0.1, run: 0.5 }
+}
 
-const ActiviyInput = ({ color, activityType }) => {
+const ActiviyInput = ({ color, activityType, threshold, handleOnChange, value }) => {
 
     return (
         <Col style={[SharedStyles.W100, SharedStyles.PH20, SharedStyles.start]}>
@@ -34,7 +37,11 @@ const ActiviyInput = ({ color, activityType }) => {
             </Text>
             <InputWithOverlayContainer>
                 <Input placeholder={'0 MIN'} fontSize={'22px'} color={color}
-                    fontWeight={'bold'} keyboardType={'numeric'} />
+                    fontWeight={'bold'}
+                    keyboardType={'numeric'}
+                    defaultValue={value.toString()}
+                    onChangeText={(value) =>
+                        handleOnChange(value, activityType.toLocaleLowerCase())} />
                 <Text
                     style={{
                         position: 'absolute',
@@ -46,13 +53,49 @@ const ActiviyInput = ({ color, activityType }) => {
                     color={color}
                     fontWeight={'bold'}
                     size={'22px'}
-                >{'30MIN'}</Text>
+                >{`${threshold}MIN`}</Text>
             </InputWithOverlayContainer>
         </Col>
     );
 }
 
-const ActivityWidgit = ({handleClose}) => {
+const ActivityWidgit = ({ handleClose }) => {
+
+    const [total, setTotal] = useState(0);
+    const [activity, setactivity] = useState({ run: 0, walk: 0, play: 0 });
+
+    useEffect(() => {
+        setactivity(rings.summary);
+    }, [])
+
+    useEffect(() => {
+        let summedUp = Object.values(rings.summary).reduce((b, v) => b + v, 0);
+        setTotal(summedUp);
+    }, [total])
+
+    useEffect(() => {
+
+    }, [activity])
+
+    const handleChangeValue = (value: number, activityType: 'run' | 'walk' | 'play') => {
+        let updated;
+        switch (activityType) {
+            case 'run': {
+                updated = { ...activity, run: value };
+                break;
+            }
+            case 'walk': {
+                updated = { ...activity, walk: value };
+                break;
+            }
+            case 'play': {
+                updated = { ...activity, play: value };
+                break;
+            }
+        }
+        setactivity(updated);
+    }
+
     return (
         <PageWrapper backgroundColor={'#fff'}>
             <Row style={[SharedStyles.PH20, SharedStyles.MT20, SharedStyles.SB]}>
@@ -61,15 +104,15 @@ const ActivityWidgit = ({handleClose}) => {
                     <CircleImage source={close} width={20} />
                 </TouchableOpacity>
             </Row>
-            {rings && <CustomActiviyRings ringsValues={rings} size={width * 0.5} strokeWidth={25} speed={2000} flex={0.4} imageSource={huskey} />}
+            {rings && <CustomActiviyRings ringsValues={rings.data} size={width * 0.5} strokeWidth={25} speed={2000} flex={0.4} imageSource={huskey} />}
             <Col style={{ flex: 0.1 }}>
                 <Text fontWeight={'bold'} size={'14px'}>{`Today's Activity`}</Text>
-                <Text fontWeight={'bold'} size={'30px'}>{`52/70MIN`}</Text>
+                <Text fontWeight={'bold'} size={'30px'}>{`${total ? total : 0}/${rings.required.total}MIN`}</Text>
             </Col>
             <StyledView style={[SharedStyles.SA, SharedStyles.MB20, { flex: 0.5 }]}>
-                <ActiviyInput color={'#FF821C'} activityType={'Run'} />
-                <ActiviyInput color={'#00BFBF'} activityType={'Walk'} />
-                <ActiviyInput color={'#48BFFD'} activityType={'Play'} />
+                <ActiviyInput color={'#FF821C'} activityType={'Run'} threshold={rings.required.total * rings.required.run } handleOnChange={handleChangeValue} value={activity.run} />
+                <ActiviyInput color={'#00BFBF'} activityType={'Walk'} threshold={rings.required.total * rings.required.walk} handleOnChange={handleChangeValue} value={activity.walk} />
+                <ActiviyInput color={'#48BFFD'} activityType={'Play'} threshold={rings.required.total * rings.required.play} handleOnChange={handleChangeValue} value={activity.play} />
             </StyledView>
         </PageWrapper>
     )
